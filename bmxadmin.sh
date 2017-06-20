7fd971398ce58e042f29560080f9cbbe4291be97
leighwilliamson <leighw@us.ibm.com>
2017-06-19 21:20:30 -0500

    update after figuring out bx cli bug

--- bmxadmin.sh
+++ bmxadmin.sh
@@ -2,12 +2,10 @@
 # requires the Bluemix CLI which can be downloaded from http://clis.ng.bluemix.net/ui/home.html
 # first argument is Bluemix "endpoint" - the URL for Bluemix
 
-endpoint="https://api.ng.bluemix.net/"
-username="leighw@us.ibm.com"
-password="vgy78uhb"
-account="e97a8c01ac694e308ef3ad7795bcf65a"
+endpoint="https://api.ng.bluemix.net"
 org="leighw@us.ibm.com"
 space="dev"
+apiKeyFile="apiKey-1.json"
 log_file="bmxadmin.log"
 
 inputfile=$1
@@ -22,18 +20,13 @@ else
 fi
 
 echo "executing: bx api " $endpoint
-#bx api $endpoint
+bx api $endpoint
 
-echo "executing: bx login " $username
-#bx login -u $username -p $password -o $org -s $space
-#bluemix login --apikey @apiKey.json -o $org -s $space
-
-
-#echo "executing bx target " $org $space
-#bx target -o $org -s $space
+echo "executing: bx login --apikey " $apiKeyFile
+bx login --apikey @$apiKeyFile -s $space
 
 echo "executing: list existing users"
-#bx iam space-users $org $space
+bx iam space-users $org $space
 
 IFS=","
 while read f1 f2 f3
@@ -44,5 +37,8 @@ do
 		if [ "$f1" != "username" ] # skip the csv file header line
 		then 
 			echo "executing: bx iam org-user-add $f1 $org"
+			bx iam org-user-add $f1 $org
+		else
+			echo "Skipping cvs file header line"
 		fi
 done < $inputfile
